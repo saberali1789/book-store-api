297504d5997e443b0382aa91f1d4a5639680e772
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2024-05-31 11:07:55 +0300

    .env file

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bookPath = require("./routes/books.js");
 const authorsPath = require("./routes/authors.js");
 const mongoose = require("mongoose");
-
+const dotenv= require('dotenv')
+dotenv.config()
 // Cnonect To Database
 mongoose
-  .connect("mongodb://127.0.0.1/bookStoreDB")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connection with Mongo"))
   .catch((error) => console.log("Faild to Connect to Mongo", error));
 
@@ -19,5 +20,5 @@ app.use("/api/books", bookPath);
 app.use("/api/authors", authorsPath);
 
 //Running Server
-const PORT = 5000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on PORT ${PORT}`));
--- routes/books.js
+++ routes/books.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const {validateCreateBook, validateUpdateBook} = require('../models/Book')
+const {
+  validateCreateBook,
+  validateUpdateBook,
+  Book,
+} = require("../models/Book");
+const asyncHandler = require("express-async-handler");
 
-const books = [
-  {
-    id: 1,
-    title: "Black Swan",
-    author: "Nassim Nicholas Taleb",
-    description: "About Black Swan",
-    price: 10,
-    cover: "soft cover",
-  },
-  {
-    id: 2,
-    title: "Skin In The Game",
-    author: "Nassim Nicholas Taleb",
-    description: "About Skin In The Game",
-    price: 12,
-    cover: "soft cover",
-  },
-  // {
-  //   id: 2,
-  //   "title": "Skin In The Game",
-  //   "author": "Nassim Nicholas Taleb",
-  //   "description": "About Skin In The Game",
-  //   "price": "12",
-  //   "cover": "soft cover",
-  // },
-];
+// const books = [
+//   {
+//     id: 1,
+//     title: "Black Swan",
+//     author: "Nassim Nicholas Taleb",
+//     description: "About Black Swan",
+//     price: 10,
+//     cover: "soft cover",
+//   },
+//   {
+//     id: 2,
+//     title: "Skin In The Game",
+//     author: "Nassim Nicholas Taleb",
+//     description: "About Skin In The Game",
+//     price: 12,
+//     cover: "soft cover",
+//   },
+//   // {
+//   //   id: 2,
+//   //   "title": "Skin In The Game",
+//   //   "author": "Nassim Nicholas Taleb",
+//   //   "description": "About Skin In The Game",
+//   //   "price": "12",
+//   //   "cover": "soft cover",
+//   // },
+// ];
 
 /**
  * @desc  Get all books
@@ -46,9 +51,13 @@ const books = [
  * @access  puplic
  */
 
-router.get("/", (req, res) => {
-  res.status(200).json(books);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const books = await Book.find().populate('author',['_id', 'firstName', 'lastName' ]);
+    res.status(200).json(books);
+  })
+);
 
 /**
  * @desc  Get book by id
@@ -56,15 +65,18 @@ router.get("/", (req, res) => {
  * @method  Get
  * @access  puplic
  */
-router.get("/:id", (req, res) => {
-  const book = books.find((book) => book.id === Number(req.params.id));
-  if (book) {
-    console.log(req.params);
-    res.status(200).json(book);
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findById(req.params.id).populate('author');
+    if (book) {
+      console.log(req.params);
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  })
+);
 
 /**
  * @desc  Create a new book
@@ -72,23 +84,25 @@ router.get("/:id", (req, res) => {
  * @method  POST
  * @access  puplic
  */
-router.post("/", (req, res) => {
-  const { error } = validateCreateBook(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { error } = validateCreateBook(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
 
-  const bookSended = {
-    id: books.length + 1,
-    title: req.body.title,
-    author: req.body.author,
-    description: req.body.description,
-    price: req.body.price,
-    cover: req.body.cover,
-  };
-  books.push(bookSended);
-  res.status(201).json(bookSended);
-});
+    const bookSended = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      description: req.body.description,
+      price: req.body.price,
+      cover: req.body.cover,
+    });
+    const result = await bookSended.save();
+    res.status(201).json(result);
+  })
+);
 
 /**
  * @desc  Update a book
@@ -96,16 +110,28 @@ router.post("/", (req, res) => {
  * @method  PUT
  * @access  puplic
  */
-router.put("/:id", (req, res) => {
-  const { error } = validateUpdateBook(req.body);
-  error && res.status(400).json({ message: error.details[0].message });
+router.put(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const { error } = validateUpdateBook(req.body);
+    error && res.status(400).json({ message: error.details[0].message });
 
-  const book = books.find((book) => book.id === parseInt(req.params.id));
-  book
-    ? res.status(200).json({ message: "Book has been updated" })
-    : res.status(404).json({ message: "Book doesn't update" });
-});
-
+    const bookUpdate = await Book.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          title: req.body.title,
+          author: req.body.author,
+          description: req.body.description,
+          price: req.body.price,
+          cover: req.body.cover,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(bookUpdate);
+  })
+);
 
 /**
  * @desc  Delete a book
@@ -113,13 +139,17 @@ router.put("/:id", (req, res) => {
  * @method  DELETE
  * @access  puplic
  */
-router.delete("/:id", (req, res) => {
-
-  const book = books.find((book) => book.id === parseInt(req.params.id));
-  book
-    ? res.status(200).json({ message: "Book has been Deleted" })
-    : res.status(404).json({ message: "Book not Found" });
-});
-
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const book = Book.findById(req.params.id);
+    if (book) {
+      await Book.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "Book has been Deleted" });
+    } else {
+      res.status(404).json({ message: "Book not Found" });
+    }
+  })
+);
 
 module.exports = router;
