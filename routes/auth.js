6f80413fa3143883ef8c620def896848244ca5da
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2024-07-01 19:26:50 +0300

    Login

--- routes/auth.js
+++ routes/auth.js
@@ -40,6 +40,37 @@ router.post(
     const token = null
 
     const { password, ...other } = result._doc;
+    res.status(201).json({...other, token});
+  })
+);
+/**
+ * @desc    Login User
+ * @route   /api/auth/login
+ * @method  POST
+ * @access  puplic
+ */
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { error } = validateLoginUser(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: "invalid email or password" });
+    }
+
+   const isPasswordMatch = await bcrypt.compare(req.body.password, user.password)
+
+   if (!isPasswordMatch) {
+    return res.status(400).json({ message: "invalid password" });
+  }
+
+      const token = null
+
+    const { password, ...other } = user._doc;
     res.status(200).json({...other, token});
   })
 );
